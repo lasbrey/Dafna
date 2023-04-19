78d939a95af8a1bf13c41fa165c90911116eaf41
Lazarus <lasbrey@macs-MacBook-Pro.local>
2023-04-19 18:03:16 +0100

    Dafna v1 comit 2

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -4,17 +4,17 @@ import { useRouter } from "next/router";
 
 // Default value for some meta data
 const defaultMeta = {
-  title: 'LaslesVPN',
-  siteName: 'LaslesVPN',
+  title: 'Dafna Consult',
+  siteName: 'Dafna Consult',
   description:
-    'Landing page VPN LaslesVPN Best VPN For Privacy, Country and Cheapest',
+    'Dafna Consult offers a wide range of advertising services',
   // change base url of your web (without '/' at the end)
-  url: 'https://next-landing-vpn.vercel.app',
+  url: 'https://dafnaconsult.com',
   type: 'website',
   robots: 'follow, index',
   // change with url of your image (recommended dimension = 1.91:1)
   // used in twitter, facebook, etc. card when link copied in tweet/status 
-  image: 'https://next-landing-vpn.vercel.app/assets/card-image.png',
+  image: '/favicon/logo.png',
   author: 'Lorem Ipsum'
 };
 
